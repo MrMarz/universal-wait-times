d17fa0aac738ab65e45d0e198c609799eef51637
MrMarz <63433755+MrMarz@users.noreply.github.com>
2026-03-16 08:56:12 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,23 +3,31 @@ import pandas as pd
 from datetime import datetime
 import os
 
-URL = "https://queue-times.com/parks/64/queue_times.json"
+parks = {
+    "Islands of Adventure": "https://queue-times.com/parks/64/queue_times.json",
+    "Universal Studios Florida": "https://queue-times.com/parks/65/queue_times.json",
+    "Epic Universe": "https://queue-times.com/parks/83/queue_times.json"
+}
 
-response = requests.get(URL)
-data = response.json()
-
-timestamp = datetime.utcnow()
+timestamp = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
 rows = []
 
-for land in data["lands"]:
-    for ride in land["rides"]:
-        rows.append({
-            "timestamp": timestamp,
-            "ride": ride["name"],
-            "wait_time": ride["wait_time"],
-            "is_open": ride["is_open"]
-        })
+for park, url in parks.items():
+
+    response = requests.get(url)
+    data = response.json()
+
+    for land in data.get("lands", []):
+        for ride in land.get("rides", []):
+
+            rows.append({
+                "timestamp": timestamp,
+                "park": park,
+                "ride": ride.get("name"),
+                "wait_time": ride.get("wait_time"),
+                "is_open": ride.get("is_open")
+            })
 
 df = pd.DataFrame(rows)
 
